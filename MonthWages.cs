0c16e38c5c9fb7d61c5bc34525201ecece4db895
mandara11 <mandaram2345@gmail.com>
2022-08-04 23:02:27 +0530

    WageForMonth

---
+++ MonthWages.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class MonthWages
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public void WagesforMonth()
+        {
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+
+            for (int day = 0; day<NUM_OF_WORKING_DAYS; day++)
+            {
+                Random obj = new Random();
+                int empCheck = obj.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee Wage: " + empWage);
+            }
+            Console.WriteLine("totalEmployee Wage: " + totalEmpWage);
+        }
+    }
+}
