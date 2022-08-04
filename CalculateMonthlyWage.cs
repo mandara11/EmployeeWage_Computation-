58e1031310ed3451a980641cd7e9446b8f4de11a
mandara11 <mandaram2345@gmail.com>
2022-08-04 23:08:28 +0530

    CalculateMonthlyEmployeeWage

---
+++ CalculateMonthlyWage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class CalculateMonthlyWage
+    {
+        //constants
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 10;
+        public void CalculateMonthlyEmpWage()
+        {
+            //Variables
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+
+            while (totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random obj = new Random();
+                int empCheck = obj.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs+=empHrs;
+                Console.WriteLine("Day : " +totalWorkingDays + "Emp Hrs:" + empHrs);
+            }
+            int totalempWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage : " + totalempWage);
+        }
+    }
+}
