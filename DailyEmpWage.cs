58a0877627a631dda5359cb0a56762a3ac2d419a
mandara11 <mandaram2345@gmail.com>
2022-08-04 22:33:44 +0530

    CheckDailyEmpWage

---
+++ DailyEmpWage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class DailyEmpWage
+    {
+        public void CheckEmployeeAttendence()
+        {
+            //constants
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random obj = new Random();
+            int empCheck = obj.Next(2);
+            if (empCheck==IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs=0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage : " + empWage);
+        }
+    }
+}
