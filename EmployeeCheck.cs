d2e1a015179edcb097f3ef4131127d71ff36476f
mandara11 <mandaram2345@gmail.com>
2022-08-04 22:13:21 +0530

    EmployeeAttendence

---
+++ EmployeeCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeCheck
+    {
+        public void CheckEmployeeAttendence()
+        {
+            int IS_FULL_TIME = 1;
+            Random obj = new Random();
+            int empCheck = obj.Next(2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
