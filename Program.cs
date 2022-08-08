e33a3f37aa7cd1fd2446848639803ea82b4c5cd2
mandara11 <mandaram2345@gmail.com>
2022-08-08 20:22:23 +0530

    Added:UC10

--- Program.cs
+++ Program.cs
@@ -5,14 +5,12 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Program");
-            
 
-            //UC9
-            SaveEachCompanyTotalWage dMart = new SaveEachCompanyTotalWage("DMart", 20, 2, 10);
-            SaveEachCompanyTotalWage reliance = new SaveEachCompanyTotalWage("Reliance", 10, 4, 20);
-            dMart.computeEmpWage();
-            Console.WriteLine(dMart.ToString());
-            Console.WriteLine(reliance.ToString());
+            //UC10
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
