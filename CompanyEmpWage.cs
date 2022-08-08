f5ff127f9b00a6a1909655235264e3680fe77894
mandara11 <mandaram2345@gmail.com>
2022-08-08 20:21:08 +0530

    Uploading Interface,empWageBuilderArray,IempComputeWage file in EmpWage

---
+++ CompanyEmpWage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+        public int numOfWorkingDays;
+
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+            this.totalEmpWage = 0;
+        }
+
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+        public string toString()
+        {
+            String myString = "Total Emp Wage for company : " + this.company + " is: " + this.totalEmpWage;
+            return myString;
+        }
+    }
+}
