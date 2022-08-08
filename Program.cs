dcd6c4adbb4b1afca6f12d98248cf0ff5d3d6c61
mandara11 <mandaram2345@gmail.com>
2022-08-08 21:41:39 +0530

    Added:UC13

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Program");
+
             CalculateMonthlyWage company = new CalculateMonthlyWage();
             company.ComputeEmpWage();
         }
