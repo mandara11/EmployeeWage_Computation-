34d13adb82e09418b3dc11de4dd646dbdce304db
mandara11 <mandaram2345@gmail.com>
2022-08-08 21:26:30 +0530

    Added:UC11

--- Program.cs
+++ Program.cs
@@ -5,12 +5,15 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Program");
-
-            //UC10
-            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
-            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
-            empWageBuilder.computeEmpWage();
+            CalculateMonthlyWage company = new CalculateMonthlyWage();
+            Console.WriteLine("Enter :\nThe Company Name\nEmpWage Working Per Hours\nTotal Working Hours\nTotal Working Days In Month");
+            string compName = Console.ReadLine();
+            int EmpWageWorkingPerHours = Convert.ToInt32(Console.ReadLine());
+            int TotalWorkingHours = Convert.ToInt32(Console.ReadLine());
+            int TotalWorkingDaysInMonth = Convert.ToInt32(Console.ReadLine());
+            company.AddCompanyEmpWage(compName, EmpWageWorkingPerHours, TotalWorkingHours, TotalWorkingDaysInMonth);
+            company.ComputeEmpWage();
+            Console.WriteLine("Total wage for" + compName + ":" + " " + company.GetTotalEmpWage(compName));
         }
     }
 }
